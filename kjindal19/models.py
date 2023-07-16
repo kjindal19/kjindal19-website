7803f02538ecc3ef392e6c1db18fa04f7e665e25
kindal19 <kunal.devloper@gmail.com>
2023-07-16 15:58:21 +0530

    Created New Models in models.py file and fixed links in the HTML template

--- kjindal19/models.py
+++ kjindal19/models.py
@@ -37,3 +37,80 @@ class User(db.Model, UserMixin):
 
     def _repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class ProjectCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+class ProjectType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    logo = db.Column(db.String(150), nullable=False, default='default.jpg')
+    description = db.Column(db.String(2000), nullable=True)
+    address = db.Column(db.String(100), nullable=False )
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.Integer, nullable = False)
+    type = db.Column(db.Integer, nullable = False)
+    client = db.Column(db.Integer, nullable = False)
+    date = db.Column(db.datetime, nullable = False)
+    description1 = db.Column(db.String(2000), nullable=True)
+    description2 = db.Column(db.String(2000), nullable=True)
+    description3 = db.Column(db.String(2000), nullable=True)
+    img1 = db.Column(db.String(150), nullable=False, default='default.jpg')
+    img2 = db.Column(db.String(150), nullable=False, default='default.jpg')
+    img3 = db.Column(db.String(150), nullable=False, default='default.jpg')
+    img4 = db.Column(db.String(150), nullable=True)
+    img5 = db.Column(db.String(150), nullable=True)
+
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    client = db.Column(db.Integer, nullable = False)
+    date = db.Column(db.datetime, nullable = False)
+    stars = db.Column(db.Integer, nullable = False)
+    text = db.Column(db.String(2000), nullable=False)
+
+class Mydetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    dob = db.Column(db.datetime, nullable = False)
+    phone = db.Column(db.String(15), nullable = False)
+    city = db.Column(db.String(30), nullable = False)
+    linkedin = db.Column(db.String(100), unique=True, nullable=False)
+    github = db.Column(db.String(100), unique=True, nullable=False)
+    leetcode = db.Column(db.String(100), unique=True, nullable=False)
+    instagram = db.Column(db.String(100), unique=True, nullable=False)
+    website = db.Column(db.String(100), unique=True, nullable=False)
+    Availablity = db.Column(db.String(100), unique=True, nullable=False)
+
+class Education(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.String(100), nullable=False)
+    sdate = db.Column(db.datetime, nullable = False)
+    edate = db.Column(db.datetime, nullable = False)
+    course = db.Column(db.String(100), nullable=False)
+    grade = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+
+class Experience(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    empolyer = db.Column(db.String(100), nullable=False)
+    sdate = db.Column(db.datetime, nullable = False)
+    edate = db.Column(db.datetime, nullable = False)
+    position = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+
+
+
